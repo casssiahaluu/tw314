ca30eaacb02e6085fd0889ca7c38ea62cacaf323
Halu de Cassia <haluanedecassia@gmail.com>
2020-05-14 01:33:21 -0300

    Adiciona estilização e fontes

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -9,26 +9,31 @@ import { ActionButton } from "../../styles/button";
 
 import { Form, Container } from "./styles";
 
-import Logo from "../../assets/logo/icon_logo.png";
+import Logo from "../../assets/logo/logo_vertical.png";
 
 function SignUp (props) {
   const [error, setError] = useState("");
   
   function onClick() {
     const token = jwt.sign({}, "tw314p@ssw0rd");
-    
+
     api.post("/users", { token: token })
-    .then(() => login(token))
-    .catch(() => setError("Ocorreu um erro ao registrar seu token T.T"));
+    .then(() => {
+      login(token);
+      props.history.push("/");
+    })
+    .catch(() => setError("Ops! Ocorreu um erro ao continuar T.T. Caso persista, entre em contato"));
   };
 
   return (
     <Container>
       <Form>
-        <img src={Logo} height="200" alt="tw314 logo" />
-        {error && <p>{error}</p>}
-
+        <img src={Logo} height="250" alt="tw314 logo" />
+        <p>
+          para continuar usando nosso aplicativo você precisa aceitar nossos <a href="/#">termos de uso</a>.<br />
+          deseja continuar?</p>
         <ActionButton onClick={() => onClick()}>aceitar e entrar</ActionButton>
+        {error && <p className="error">{error}</p>}
       </Form>
     </Container>
   );
