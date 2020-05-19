0888a87828b1d1daa751aad1e16b2ac8dd8fc730
Halu de Cassia <haluanedecassia@gmail.com>
2020-05-19 16:34:26 -0300

    Adiciona arquivos e pastas para about

---
+++ src/pages/About/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+import {
+  blockElement
+} from "../../styles/helpers";
+
+export const Container = styled.div`
+  ${blockElement}
+
+  width: 100%;
+  margin: 0 auto;
+  cursor: pointer;
+`;
