d53e2a5f85e1789ba910fc2dbe6d6dbfcbdb30a1
Halu de Cassia <haluanedecassia@gmail.com>
2020-05-21 00:52:24 -0300

    Adiciona rota para histórico

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { isAuthenticated } from "./services/auth";
 
 import About from "./pages/About";
 import Help from "./pages/Help";
+import Historic from "./pages/Historic";
 import SignUp from "./pages/SignUp";
 
 function PrivateRoute({ children, ...rest }) {
@@ -28,8 +29,9 @@ const Routes = () => (
       <Switch>
         <Route exact path="/"><SignUp /></Route>
         <PrivateRoute path="/app"><h1>App</h1></PrivateRoute>
-        <PrivateRoute path="/help"><Help /></PrivateRoute>
         <PrivateRoute path="/about"><About /></PrivateRoute>
+        <PrivateRoute path="/help"><Help /></PrivateRoute>
+        <PrivateRoute path="/historic"><Historic /></PrivateRoute>
         <Route path="*"><h1>Page not found</h1></Route>
       </Switch>
     </React.Suspense>
