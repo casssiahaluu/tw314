d609fef133b1637ece21fc1506cc468cf934c2b6
Halu de Cassia <haluanedecassia@gmail.com>
2020-05-23 19:41:42 -0300

    Adiciona arquivos para AddTicket

---
+++ src/pages/AddTicket/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import api from "../../services/api";
+import { getJwt, getToken, login } from "../../services/auth";
+
+import { ActionButton } from "../../styles/button";
+
+import { Form, Container } from "./styles";
+
+import Logo from "../../assets/logo/logo_vertical.png";
+
+export default function AddTicket (props) {
+  const [error, setError] = useState("");
+  const [info, setInfo] = useState("");
+  
+  function onClick() {
+    setInfo("Carregando. Aguarde...");
+
+    if(getToken()) {
+      window.location.href = "/app";
+      console.log("Já existe token. Redirecionando para o App");
+    } else {
+      console.log("Não há token. Criando e redirecionando para o App");
+      api.post("/users", {
+        token: getJwt()
+      }).then(response => {
+        login(getJwt());
+        window.location.href = "/app";
+        console.log("Token criado com sucesso! \\o/ Bem-vindx, nerd!");
+        setInfo("Feito! \\o/ Entrando no aplicativo");
+      }).catch(() => setError("Ops! Ocorreu um erro ao continuar T.T. Caso persista, entre em contato"));
+    }
+  };
+
+  return (
+    <Container>
+      <Form>
+        <img src={Logo} height="250" alt="tw314 logo" />
+        <p>
+          para continuar usando nosso aplicativo você precisa aceitar nossos <a href="/#">termos de uso</a>.<br />
+          deseja continuar?</p>
+        <ActionButton onClick={() => onClick()}>aceitar e entrar</ActionButton>
+        {info && <p>{info}</p>}
+        {error && <p className="error">{error}</p>}
+      </Form>
+    </Container>
+  );
+}
---
+++ src/pages/AddTicket/styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+import backgroundImage from "../../assets/images/backgroundFade-min.jpg";
+import { errorTextColor, linkTextColor} from "../../styles/colors";
+import {centeredElement} from "../../styles/helpers";
+
+export const Container = styled.div`
+  ${centeredElement}
+  height: 100vh;
+  background-image: url(${backgroundImage});
+`;
+
+export const Form = styled.div`
+  display: flex;
+  max-width: 400px;
+  align-items: center;
+  flex-direction: column;
+
+  img {
+    margin: 10px 0 15px;
+  }
+
+  p {
+    font-size: 1rem;
+    text-align: center;
+    margin-bottom: 15px;
+
+    &.error{
+      font-size: 0.85rem;
+      text-align: center;
+      color: ${errorTextColor};
+    }
+  }
+
+  a {
+    font-size: 1rem;
+    color: ${linkTextColor};
+    text-decoration: underline;
+
+    &:hover {
+      color: darken(10%, ${linkTextColor});
+    }
+  }
+`;
