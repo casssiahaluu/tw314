e1acec529d5df6cc4bf5c6cc58d60d3e19a66d0b
Halu de Cassia <haluanedecassia@gmail.com>
2020-05-25 22:19:42 -0300

    Adciona arquivos para App

---
+++ src/pages/App/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Page from "../../components/Page";
+import { Container, ResponsiveImg } from "./styles";
+
+import Logo from "../../assets/logo/logo_vertical.png";
+
+export default function App () {
+  return (
+    <Page>
+      <Container>
+        <div>
+          <ResponsiveImg src={Logo} alt="tw314 logo" />
+        </div>
+        <div>
+          <p>
+            versão 1.0.0 <br />
+            2020 ©
+          </p>
+        </div>
+        <div>
+          <a href="mailto:haluanedecassia@gmail.com">deixar feedback</a><br />
+          <a href="/help">ajuda</a>
+        </div>
+      </Container>
+    </Page>
+  );
+}
---
+++ src/pages/App/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+import { centeredElement } from "../../styles/helpers";
+
+export const Container = styled.div`
+  ${centeredElement}
+  height: 100%;
+  flex-wrap: wrap;
+  text-align:  center;
+  flex-direction: column;
+`;
+
+export const ResponsiveImg = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 235px;
+`;
