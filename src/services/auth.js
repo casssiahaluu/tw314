e116b1b50a842c497db3e96bda265082ea4bf280
Halú de Cássia <halu.amancio@imusics.com>
2020-06-14 19:25:32 -0300

    Adiciona novos helpsers para gravação em LocalStorrage

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 export const TOKEN_KEY = "@tw314-Token";
+export const ID_KEY = "@tw314-ID";
+export const TICKET_KEY = "@tw314-ticket";
+
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
+
 export const getJwt = () => jwt.sign({}, "tw314p@ssw0rd");
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
+
+export const setId = id => localStorage.setItem(ID_KEY, id);
+export const getId = () => localStorage.getItem(ID_KEY);
+
+export const setTicket = ticket => localStorage.setItem(TICKET_KEY, ticket);
+export const getTicket = () => localStorage.getItem(TICKET_KEY);
+
 export const login = token => localStorage.setItem(TOKEN_KEY, token);
 export const logout = () => localStorage.removeItem(TOKEN_KEY);
