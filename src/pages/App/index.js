94e14bc4de69218a0c604c39817ee40080a744fe
Halú de Cássia <halu.amancio@imusics.com>
2020-06-16 21:00:36 -0300

    Adiciona busca de informações para página principal

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -9,11 +9,11 @@ import Page from "../../components/Page";
 import { Container } from "./styles";
 
 export default function App () {
-  const [ticketInfo, setTicketInfo] = React.useState([]);
+  const [ticketInfo, setTicketInfo] = React.useState(undefined);
   const [place, setPlace] = React.useState("");
 
   function getColors() {
-    if (ticketInfo.position <=5) {
+    if (ticketInfo.position <= 5) {
       return "danger fas fa-hourglass-end"
     } else if (ticketInfo.position > 5 && ticketInfo.position <= 10) {
       return "warn fas fa-hourglass-half";
@@ -32,20 +32,24 @@ export default function App () {
   return (
     <Page>
       <Container>
-        <div>
-          <p className="ticket">
-            sua senha é: <span className="pass">{ticketInfo.ticket}</span>
-          </p>
-        </div>
-        <div>
-          <i className={`hourglass ${getColors()} `}></i>
-          <p className="details">
-            <span className="message">ATENÇÃO</span> <br />
-            você está na <span className="people">{ticketInfo.position}ª posição</span> da fila <br/>
-            tempo estimado: <span className="time">{ticketInfo.waitingTime} min</span> <br />
-            local: <span className="place">{place}</span>
-          </p>
-        </div>
+        {ticketInfo ? (
+          <React.Fragment>
+            <div>
+              <p className="ticket">
+                sua senha é: <span className="pass">{ticketInfo.ticket}</span>
+              </p>
+            </div>
+            <div>
+              <i className={`hourglass ${getColors()} `}></i>
+              <p className="details">
+                <span className="message">ATENÇÃO</span> <br />
+                você está na <span className="people">{ticketInfo.position}ª posição</span> da fila <br/>
+                tempo estimado: <span className="time">{ticketInfo.waitingTime} min</span> <br />
+                local: <span className="place">{place}</span>
+              </p>
+            </div>
+          </React.Fragment>
+        ): <h3>não há nenhum ticket aguardado aqui! <br /><small>vá para <a href="/add-ticket">adicionar ticket</a> para acompanhar um</small></h3>}
       </Container>
     </Page>
   );
