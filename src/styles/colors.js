962c571c7f739f89ccd2509d858686d0f2d943b8
Halú de Cássia <halu.amancio@imusics.com>
2020-06-17 22:08:13 -0300

    Adiciona cores de aceito e perigo a paleta

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -5,6 +5,7 @@ export const lightColor = "#FFFEFA";
 export const darkColor = '#5E4343';
 export const linkTextColor = "#0974E6";
 export const errorTextColor = "#C80B47";
+export const succesTextColor = "#1A9F1A";
 export const contrastTextColor = "#DB6622";
 
 export const contrastMessageTextColor = "#BB7D11";
@@ -16,3 +17,5 @@ export const contrastPlaceTextColor = "#438BFF";
 export const starsIconColor = "#ECB900";
 
 export const bgDefaultButton = "linear-gradient(to right, #FFBF00, #F2913D, #FF5E00)";
+export const bgDangerButton = "linear-gradient(to right, #fd0050,#C80B47);";
+export const bgSuccessButton = "linear-gradient(to right, #62ff62, #1A9F1A)";
