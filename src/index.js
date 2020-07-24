0dda19afa93f4f175dec460e845e3ede7868e282
Halú de Cássia <halu.amancio@imusics.com>
2020-07-24 20:46:32 -0300

    Atualiza yarn.lock

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ import App from './App';
 
 import { initializeFirebase } from './push-notification';
 
-import imagemin from 'imagemin';
-import imageminWebp from 'imagemin-webp';
-
-(async () => {
-	await imagemin(['assets/*.{jpg,png}'], {
-		destination: 'build/images',
-		plugins: [
-			imageminWebp({quality: 50})
-		]
-	});
-
-	console.log('Images optimized');
-})();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 initializeFirebase();
